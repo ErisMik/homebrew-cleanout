f9d919c1a3336f497983c873e621f9f04a5a054c
Eric Mikulin <ericm99@gmail.com>
2016-05-21 00:54:35 -0600

    Improves verbosity and fixes a major bug
    Adds many output statements for better transparency and reliability during operation. Also fixes a bug that would delete dependencies that were currently in use.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homebrew-cleanout
+
 A python script that assists with cleaning out unused dependencies
+
+## How to Use
+
+Download the script, run it and follow the prompts. Pu the script in a safe directory if you want your marked packages to stay marked.
+
+## License
+
+N/A yet
--- brewCleanout.py
+++ brewCleanout.py
@@ -4,35 +4,42 @@ import subprocess
 from os import system
 
 def main():
-    # Find all the packeges the user has installed
+    # Find all the packages the user has installed by calling 'brew list'
+    print("Finding installed packages")
     packages = []
     proc = subprocess.Popen(["brew", "list"], stdout=subprocess.PIPE)
     tmp = proc.stdout.read()
     tmp = tmp.split()
     for item in tmp:
         packages.append(item)
-    print packages
+    print(packages)
 
-    # Determine wich ones are deletable candidates
+    # Determine which packages are deletable candidates
     possibles = []
     for formula in packages:
-        # proc = subprocess.Popen(["brew", "uses", "--installed", formula], stdout=subprocess.PIPE)
-        # tmp = proc.stdout.read()
-
+        print("Finding uses and dependencies for "+formula)
         proc2 = subprocess.Popen(["brew", "uses", formula], stdout=subprocess.PIPE)
         tmp2 = proc2.stdout.read()
 
-        inter = list(set(packages) & set(tmp2))
+        tmp2 = tmp2.split("\n")
 
-        # if tmp == "" and not tmp2 == "":
-        if len(inter) == 0 and not tmp2 == "":
+        inter = []
+        for dep in tmp2:
+            if dep in packages:
+                inter.append(dep)
+
+        if len(inter) == 0 and not tmp2[0] == '':
             possibles.append(formula)
+            print("Adding "+formula+" as deletion candidate")
 
-    # Ensure there are packages to delete
+    # Ensure there are actually packages to delete
     if len(possibles) == 0:
+        print("No unused dependencies found")
         return
+    print("Unused dependencies found")
 
-    # Find descriptions of the packages
+    # Find descriptions of the packages for use when deleting
+    print("Finding package descriptions")
     deps = {}
     command = ["brew", "desc"] + possibles
     proc = subprocess.Popen(command, stdout=subprocess.PIPE)
@@ -43,25 +50,40 @@ def main():
         tups = line.split(":")
         deps[tups[0]] = tups[1].strip()
 
-    # Ask for deletion of all the packages
-    toDelete = []
+    # Ask for deletion of each of the packages
+    print("")
+    to_delete = []
     for formula in deps:
-        print formula + ": " + deps[formula]
-        value = raw_input("Delete %s ? [Y/n]" % formula)
+        print(formula + ": " + deps[formula])
+        value = raw_input("Delete %s? [Y/n]" % formula)
         if "n" not in value:
-            toDelete.append(formula)
+            to_delete.append(formula)
+        print("")
 
-    # delete packages
-    for package in toDelete:
-        print "DELETING: " + package
+    # Delete packages
+    for package in to_delete:
+        print("DELETING: " + package)
         system("brew uninstall %s" % package)
+        print("Done\n")
 
-    # Ask for cleanup
-    value = raw_input("Perform 'brew cleanup' ? [Y/n]")
+    # Ask to run 'brew cleanup'
+    value = raw_input("Perform 'brew cleanup -s -force'? [Y/n]")
     if "n" not in value:
-        print "Performing cleanup"
-        system("brew cleanup")
+        print("Performing cleanup")
+        system("brew cleanup -s -force")
+    else:
+        value = raw_input("Perform 'brew cleanup'? [Y/n]")
+        if "n" not in value:
+            print("Performing cleanup")
+            system("brew cleanup")
+
+    # Ask to run 'brew doctor'
+    value = raw_input("Perform 'brew doctor'? [Y/n]")
+    if "n" not in value:
+        print("Performing doctor")
+        system("brew doctor")
 
 
 if __name__ == "__main__":
     main()
+    print("Homebrew Cleanout complete, exiting....")
